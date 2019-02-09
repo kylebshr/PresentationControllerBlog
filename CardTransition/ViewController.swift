27049ad2418305745e59441893427af31bbc9466
Kyle Bashour <kylebshr@me.com>
2019-02-08 19:16:00 -0800

    Initial Commit

---
+++ CardTransition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardTransition
+//
+//  Created by Kyle Bashour on 2/8/19.
+//  Copyright © 2019 Kyle Bashour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
